3fa82b2d6c5e0061b0776c3ae89768b9cb8d1fbe
Dale Hagen <mathematiqueNageur@users.noreply.github.com>
2023-02-20 19:30:46 -0500

    Updated Navigation Bars

--- itis3153/scripts/hfscripts.js
+++ itis3153/scripts/hfscripts.js
@@ -1,7 +1,7 @@
+// Header Function
 $(function() {
-    console.log("made it here");
     const pathname = String(location.pathname.split("/").splice(-1));
-    console.log(pathname);
+    
     const links = {
         "1": {
             "url": "index.html",
@@ -29,14 +29,19 @@ $(function() {
             "alt": "Forms Page"
         },
         "6": {
-            "url": "template.html",
-            "text": "Template",
-            "alt": "Template Page"
+            "url": "website_evaluations.html",
+            "text": "Website Evaluations",
+            "alt": "Website Evaluations Page"
+        },
+        "7": {
+            "url": "firstscripts.html",
+            "text": "First Scripts",
+            "alt": "First Scripts Page"
         }
     };
 
-    const linkSeperator = " | ";
-    let htmlString = "";
+    const linkSeperator = "] [";
+    let htmlString = "[";
 
     for (var id in links) {
         if (pathname === links[id]['url']) {
@@ -46,8 +51,61 @@ $(function() {
         }
         if(Number(id) < Object.keys(links).length) {
             htmlString += linkSeperator;
+        } else {
+            htmlString += ']'
         }
     }
 
     $('#headerSiteLinks').html(htmlString);
+});
+
+$(function() {
+    const pathname = String(location.pathname.split("/").splice(-1));
+    
+    const links = {
+        "1": {
+            "url": "https://webpages.charlotte.edu/dhagen3/",
+            "text": "CIT Webspace",
+            "alt": "CIT Webspace"
+        },
+        "2": {
+            "url": "https://github.com/mathematiqueNageur",
+            "text": "My GitHub",
+            "alt": "My GitHub Home"
+        },
+        "3": {
+            "url": "https://mathematiqueNageur.github.io",
+            "text": "GitHub.io",
+            "alt": "mathematiqueNageur's GitHub.io Page"
+        },
+        "4": {
+            "url": "https://www.freecodecamp.org/dxhagen",
+            "text": "freecodecamp",
+            "alt": "My freecodecamp Page"
+        },
+        "5": {
+            "url": "https://www.codecademy.com/profiles/dxhagen",
+            "text": "Codecademy",
+            "alt": "My Codecademy Page"
+        },
+        "6": {
+            "url": "https://www.linkedin.com/in/dalehagen/",
+            "text": "LinkedIn",
+            "alt": "My LinkedIn Page"
+        }
+    };
+
+    const linkSeperator = "] [";
+    let htmlString = "[";
+
+    for (var id in links) {
+        htmlString += '<a href="'+links[id]['url']+'" alt="'+links[id]['alt']+'">'+links[id]['text']+'</a>';
+        if(Number(id) < Object.keys(links).length) {
+            htmlString += linkSeperator;
+        } else {
+            htmlString += ']'
+        }
+    }
+
+    $('#footerLinks').html(htmlString);
 });
